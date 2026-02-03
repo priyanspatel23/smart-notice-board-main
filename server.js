e348e84cdbd3413ac55cff8a789cb08a138f517d
Priyans Patel <priyansbhai250@gmail.com>
2026-02-02 23:04:55 -0800

    Add files via upload

--- server.js
+++ server.js
@@ -71,6 +71,7 @@ app.use('/admin', require('./routes/admin'));
 // app.use('/admin/notices', require('./routes/notices'));
 
 
-app.listen(3000, () => {
-    console.log(`Server running on port 3000`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
